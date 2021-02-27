49bbca8811009851e4169a1d05a6a903ce25df84
Hunter Posey <ihposey@crimson.ua.edu>
2021-02-26 23:24:38 -0800

    user controller minor change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :username, :email, :password)
+        params.require(:user).permit(:name, :username, :password, :email)
     end
 end
